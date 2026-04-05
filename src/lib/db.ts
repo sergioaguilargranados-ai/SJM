b43948804c52afd7b4ef6c6cea436eeb91a9b22d
sergioaguilargranados-ai <sergio.aguilar.granados@gmail.com>
2026-04-04 22:49:09 -0600

    v1.007 - FIX: Bypass de Neon Serverless en Vercel Build Phase

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,5 +2,6 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
 
-const sql = neon(process.env.DATABASE_URL!);
+// Fallback vacío para que Next.js "Build Time" no truene si Vercel aún no tiene la clave
+const sql = neon(process.env.DATABASE_URL || "postgresql://dummy:dummy@localhost/dummy");
 export const db = drizzle(sql, { schema });
